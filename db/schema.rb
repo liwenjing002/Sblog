fa5cc3d20b10b1424d0737f8b89c62b5f2ef8a9a
0612800232 <liwenjingabc@126.com>
2010-12-08 21:48:17 +0800

    fix db bug

--- db/schema.rb
+++ db/schema.rb
@@ -11,51 +11,5 @@
 
 ActiveRecord::Schema.define(:version => 20101207075835) do
 
-  create_table "blogs", :force => true do |t|
-    t.string   "title"
-    t.string   "text"
-    t.integer  "owner_id"
-    t.string   "blog_type"
-    t.integer  "replay_count"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "delayed_jobs", :force => true do |t|
-    t.integer  "priority",   :default => 0
-    t.integer  "attempts",   :default => 0
-    t.text     "handler"
-    t.text     "last_error"
-    t.datetime "run_at"
-    t.datetime "locked_at"
-    t.datetime "failed_at"
-    t.string   "locked_by"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
-
-  create_table "tags", :force => true do |t|
-    t.string   "text"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "tags_in_blogs", :force => true do |t|
-    t.integer  "blog_id"
-    t.integer  "tag_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "users", :force => true do |t|
-    t.string   "login_name"
-    t.string   "password"
-    t.string   "alias_name"
-    t.string   "email"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
+ 
 end
